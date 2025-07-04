2d3585656b2c0c1bdaac6459e1dcace697d43ae5
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-07-04 21:02:35 +0530

    done token part

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 import { connectdb } from "@/lib/mongodb";
 import User from "@/models/user";
+import jwt from "jsonwebtoken";
+import { cookies } from "next/headers";
 
 export async function POST(request: NextRequest) {
   try {
@@ -26,12 +28,29 @@ export async function POST(request: NextRequest) {
         profilePicture: profilePhoto,
         referal: invitelink,
       });
+
       await user.save();
+      const tokenPayload = {
+        username,
+      };
+      const token = jwt.sign(tokenPayload, process.env.JWT_SECRET as string, {
+        issuer: "Aditya Rawat",
+      });
+      (await cookies()).set("token", token, {
+        maxAge: 7 * 60 * 60 * 24,
+        httpOnly: true,
+        path: "/",
+        sameSite: true,
+      });
       return NextResponse.json(
         { success: true, message: "login successfully" },
         { status: 200 }
       );
     }
+    const tokenPayload = {
+      username,
+    };
+
     const isPasswordtrue = await bcrypt.compare(planepassword, user?.password);
     if (!isPasswordtrue) {
       return NextResponse.json(
@@ -39,6 +58,15 @@ export async function POST(request: NextRequest) {
         { status: 401 }
       );
     }
+    const token = jwt.sign(tokenPayload, process.env.JWT_SECRET as string, {
+      issuer: "Aditya Rawat",
+    });
+    (await cookies()).set("token", token, {
+      maxAge: 7 * 60 * 60 * 24,
+      httpOnly: true,
+      path: "/",
+      sameSite: true,
+    });
     return NextResponse.json(
       { success: true, message: "login successfully" },
       { status: 200 }
